1407c020265f7e5ace6fcbecd141f42bae0e2ff8
JPBG-USP <joao_garciajp@usp.br>
2026-02-12 14:39:39 -0300

    Adicionando codigo em c++

---
+++ cpp/soma_10.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int num;
+
+    std::cout << "Insira um numero para somar com 10: ";
+    std::cin >> num;
+
+    std::cout << "10 + " << num << " = " << (10 + num) << std::endl;
+
+    return 0;
+}
